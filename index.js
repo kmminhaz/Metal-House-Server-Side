2b05c60a00ac73eda6361aa4d5d9b48c5a233d1d
kmminhaz <kmminhaj.khan@gmail.com>
2022-05-24 03:56:19 +0600

    Server is connected to MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// username : kmminhaz
-// password: TKjcWRT889pogfXW
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -10,10 +8,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to Metal House');
+app.get("/", (req, res) => {
+  res.send("Welcome to Metal House");
 });
 
+// Database Connection
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xlwxjjl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // const toolCollection = client.db("metalwork").collection("tools");
+
+    await client.db("tools").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
 app.listen(port, () => {
-    console.log(`Metal-House Server is Running on the port ${port}`);
+  console.log(`Metal-House Server is Running on the port ${port}`);
 });
