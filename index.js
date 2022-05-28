aa638ba8309b97dfdb3ad574bbfc65ac92d63e43
kmminhaz <kmminhaj.khan@gmail.com>
2022-05-28 10:33:29 +0600

    API for Delete Order

--- index.js
+++ index.js
@@ -56,6 +56,13 @@ async function run() {
         res.send(products);
       }
     });
+
+    app.delete("/myOrders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
