8d291128ce2153a014cd6e08e1ae6745d101189d
kmminhaz <kmminhaj.khan@gmail.com>
2022-05-28 23:38:20 +0600

    API to get ALL Orders

--- index.js
+++ index.js
@@ -163,6 +163,12 @@ async function run() {
       res.send(result);
     });
     
+    app.get("/orders", async (req, res) => {
+      const query = {};
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
 
   } finally {
     // await client.close();
