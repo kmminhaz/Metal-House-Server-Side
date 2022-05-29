54c9f1fec2673cfa40cd6fa5ec1cbf5ae6ffd8c0
kmminhaz <kmminhaj.khan@gmail.com>
2022-05-29 07:48:37 +0600

    All Api is Set

--- index.js
+++ index.js
@@ -199,6 +199,13 @@ async function run() {
       );
       res.send(result);
     });
+
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
   } finally {
     // await client.close();
   }
